6d8b6cf18d9a4988168714ccd71e0eda51bf4152
fastlearner1110 <95926942+fastlearner1110@users.noreply.github.com>
2022-04-26 20:29:29 +0530

    Add files via upload

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,9 @@
 import React, { Component, useState } from "react";
 import '../styles/App.css';
 
-class App extends Component {
-  constructor(props) {
-    super(props)
+function App() {
 
-    this.cityList = [{ name: 'Goa', country: 'India' },
+    cityList = [{ name: 'Goa', country: 'India' },
     { name: 'Amsterdam', country: 'Netherlands' },
     { name: 'New York', country: 'USA' },
     { name: 'Darjeeling', country: 'India' },
@@ -35,21 +33,24 @@ class App extends Component {
     { name: 'Mount Abu', country: 'India' },
     { name: 'Tirupati', country: 'India' },
     ]
-  }
+  
+    let cityListIndian=cityList.filter((ele,index)=>{
+        return ele.country==="India";
+    })
 
-  render() {
+
     return (
       <div id="main">
         {/* Do not remove the main div */}
         <ol>
-          {this.cityList.map((ele,index)=>{
-            return <li key='location${index+1}'>{ele.included("India").name}({ele.included("India").country})</li>
+          {cityListIndian.map((ele,index)=>{
+            return <li key='location${index+1}'>{ele.name}({ele.country})</li>
           })}
         </ol>
       </div>
     )
   }
-}
+
 
 
 export default App;
